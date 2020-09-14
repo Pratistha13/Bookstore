d4f0f24b9d6fd40062902ef985fd289a6c4e2db7
P1748 <p1748@student.jamk.fi>
2020-09-14 11:37:52 +0300

    dependencies added

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,13 +1,32 @@
 package com.example.Bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args)-> {
+			Book b1 = new Book("Saaya", "Subin",2015, "DESF678", 23.78);
+			Book b2 = new Book("Maya", "Bhairab",2007, "BASF965", 30.00);
+			Book b3 = new Book("Captain", "Jack", 1987, "JACSP8", 19.90);
+			
+			repository.save(b1);
+			repository.save(b2);
+			repository.save(b3);
+			
+		};
 	}
 
 }
--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,27 +1,20 @@
 package com.example.Bookstore.web;
 
-import java.util.ArrayList;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
-	
-	@RequestMapping("/index")
-	public String bookInfo(Model model) {
-		ArrayList <Book> bookList = new ArrayList <Book>();
-		
-		Book book1 = new Book("Saaya", "Subin Bhattarai", 2015, 23456, 45.90);
-		
-		bookList.add(book1);	
-				
-		
-		return "index";
-		
+	@Autowired
+	private BookRepository repository;
+		@RequestMapping("/index")
+		public String studentList(Model model) {
+			model.addAttribute("books", repository.findAll());
+			return "index";
 	}
 
 }
