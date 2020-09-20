8d92a5e63842657ec0db0ae660cacd100d2d72a4
P1748 <p1748@student.jamk.fi>
2020-09-20 23:32:05 +0300

    oneToMany relationship between Book and Category

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,5 +1,7 @@
 package com.example.Bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,25 +9,35 @@ import org.springframework.context.annotation.Bean;
 
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
+import com.example.Bookstore.domain.Category;
+import com.example.Bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 	
 	@Bean
-	public CommandLineRunner demo(BookRepository repository) {
-		return (args)-> {
-			Book b1 = new Book("Saaya", "Subin",2015, "DESF678", 23.78);
-			Book b2 = new Book("Maya", "Bhairab",2007, "BASF965", 30.00);
-			Book b3 = new Book("Captain", "Jack", 1987, "JACSP8", 19.90);
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository) {
+		return (args) -> {
+			log.info("save a couple of books");
+			crepository.save(new Category("IT"));
+			crepository.save(new Category("Business"));
+			crepository.save(new Category("Law"));
+			crepository.save(new Category("Fiction"));
 			
-			repository.save(b1);
-			repository.save(b2);
-			repository.save(b3);
 			
+			repository.save(new Book("Harry Potter", "JK Rowling", 2007,"SJEB234", 120.00,  crepository.findByName("Fiction").get(0)));
+			repository.save(new Book("Business Rules 101", "Kateson", 1987, "BUS1013",35.00, crepository.findByName("Business").get(0)));	
+			
+			log.info("fetch all students");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+
 		};
 	}
 
--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,7 +1,5 @@
 package com.example.Bookstore.web;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
+import com.example.Bookstore.domain.CategoryRepository;
 
 
 @Controller
@@ -18,19 +17,25 @@ public class BookController {
     
 	@Autowired
 	private BookRepository repository;
+	
+	@Autowired
+	private CategoryRepository crepository; 
 
 	
-    @RequestMapping(value="/booklist")
+    @RequestMapping(value={"/booklist"})
     public String bookList(Model model) {	
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
-    
+
+ 
     @RequestMapping(value = "/add")
     public String addBook(Model model){
-    	model.addAttribute("book", new Book());
+    	model.addAttribute("books", new Book());
+    	model.addAttribute("categories", crepository.findAll());
         return "addbook";
-    }  
+    } 
+   
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String save(Book book){
         repository.save(book);
@@ -43,8 +48,8 @@ public class BookController {
     }    
     @RequestMapping(value = "/modify/{id}", method = RequestMethod.GET)
     public String modifyBook(@PathVariable("id") Long bookId, Model model) {
-    	Optional <Book> book = repository.findById(bookId);
-    	model.addAttribute("book", book);
+    	model.addAttribute("books", repository.findById(bookId));
+    	model.addAttribute("categories", crepository.findAll());
         return "modifybook";
     }   
 
